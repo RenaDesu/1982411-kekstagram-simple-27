4f08263aa38d0bc4884fe6f62f52104f9d23d8a9
RenaDesu <rena-desu@mail.ru>
2022-10-18 16:08:45 +0300

    Наполняет контентом блок с фотографиями

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {getRandomIntInclusive, checkStringLength} from './util.js';
 import {createDescriptions} from './data.js';
+import './gallery.js';
 
 console.log(getRandomIntInclusive());
 console.log(checkStringLength());
